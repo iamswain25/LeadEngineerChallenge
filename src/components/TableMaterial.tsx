25261ae9201eebdc775bf93706d550afba6f91a7
swain <iamswain@hotmail.com>
2021-03-14 17:34:56 +0900

    feat: Search by only id or SSN

--- src/components/TableMaterial.tsx
+++ src/components/TableMaterial.tsx
@@ -14,16 +14,26 @@ export default function TableMaterial() {
         icons={tableIcons}
         options={{ pageSize: 20 }}
         columns={[
-          { title: "id", field: "id" },
-          { title: "first_name", field: "first_name" },
-          { title: "last_name", field: "last_name" },
-          { title: "email", field: "email" },
-          { title: "gender", field: "gender" },
-          { title: "ethnicity", field: "ethnicity" },
-          { title: "birthdate", field: "birthdate" },
-          { title: "ssn", field: "ssn" },
-          { title: "insured", field: "insured", type: "boolean" },
-          { title: "deceased", field: "deceased", type: "boolean" },
+          { title: "id", field: "id", searchable: true },
+          { title: "first_name", field: "first_name", searchable: false },
+          { title: "last_name", field: "last_name", searchable: false },
+          { title: "email", field: "email", searchable: false },
+          { title: "gender", field: "gender", searchable: false },
+          { title: "ethnicity", field: "ethnicity", searchable: false },
+          { title: "birthdate", field: "birthdate", searchable: false },
+          { title: "ssn", field: "ssn", searchable: true },
+          {
+            title: "insured",
+            field: "insured",
+            type: "boolean",
+            searchable: false,
+          },
+          {
+            title: "deceased",
+            field: "deceased",
+            type: "boolean",
+            searchable: false,
+          },
           // {
           //   title: "diagnosis",
           //   field: "diagnosis",
