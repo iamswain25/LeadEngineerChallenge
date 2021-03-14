cf144c3255b5be592b684547e71cd1563542677e
swain <iamswain@hotmail.com>
2021-03-14 20:39:19 +0900

    ENV server port variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import TableMaterial from "./components/TableMaterial";
-
+const REACT_APP_SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
 function App() {
   const [data, setData] = React.useState();
   React.useEffect(() => {
-    fetch("http://localhost:3001/patients")
+    fetch(`http://localhost:${REACT_APP_SERVER_PORT}/patients`)
       .then((res) => res.json())
       .then(setData);
   }, [setData]);
